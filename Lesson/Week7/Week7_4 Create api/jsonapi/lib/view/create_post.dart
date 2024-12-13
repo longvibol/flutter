dd873fb1e18042f98a9ab05da58fd05ecd2184ae
longvibol <vibollong@gmail.com>
2024-12-13 16:18:44 +0700

    week8_2

--- Lesson/Week7/Week7_4 Create api/jsonapi/lib/view/create_post.dart
+++ Lesson/Week7/Week7_4 Create api/jsonapi/lib/view/create_post.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class CreatePostView extends StatelessWidget {
   CreatePostView({super.key});
--- Lesson/Week7/Week7_4 Create api/jsonapi/lib/view/home_view.dart
+++ Lesson/Week7/Week7_4 Create api/jsonapi/lib/view/home_view.dart
@@ -4,6 +4,7 @@ import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:jsonapi/view/create_post.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -18,6 +19,16 @@ class _HomeViewState extends State<HomeView> {
     final response = await http.get(Uri.parse(url));
     final posts = jsonDecode(response.body);
     return posts;
+  }
+
+  Future<void> deletPosts(String id) async {
+    final response =
+        await http.delete(Uri.parse("http://172.19.64.1:3000/posts/$id"));
+    if (response.statusCode == 200) {
+      print("Post deleted");
+    } else {
+      print("Failed to deleted");
+    }
   }
 
   @override
@@ -49,20 +60,50 @@ class _HomeViewState extends State<HomeView> {
                 itemCount: snapshot.data!.length,
                 itemBuilder: (context, index) {
                   final post = snapshot.data![index];
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "${post['title']}",
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      Text("${post['body']}"),
-                      Image.network(
-                        post['image_url'],
-                        fit: BoxFit.cover,
-                        width: double.infinity,
-                      ),
-                    ],
+                  return Slidable(
+                    endActionPane: ActionPane(
+                      motion: const ScrollMotion(),
+                      children: [
+                        // Container(
+                        //   width: 100,
+                        //   decoration: BoxDecoration(color: Colors.red),
+                        //   child: Column(
+                        //     mainAxisAlignment: MainAxisAlignment.center,
+                        //     crossAxisAlignment: CrossAxisAlignment.center,
+                        //     children: [
+                        //       Icon(Icons.delete),
+                        //       Text("Delete"),
+                        //     ],
+                        //   ),
+                        // ),
+                        SlidableAction(
+                          onPressed: (context) {
+                            // perform delete
+                            deletPosts(post['id']);
+                            setState(() {});
+                          },
+                          backgroundColor: Color(0xFFFE4A49),
+                          foregroundColor: Colors.white,
+                          icon: Icons.delete,
+                          label: 'Delete',
+                        ),
+                      ],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "${post['title']}",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        Text("${post['body']}"),
+                        Image.network(
+                          post['image_url'],
+                          fit: BoxFit.cover,
+                          width: double.infinity,
+                        ),
+                      ],
+                    ),
                   );
                 },
               ),

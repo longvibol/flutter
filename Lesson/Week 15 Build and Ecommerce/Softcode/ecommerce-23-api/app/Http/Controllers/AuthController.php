25d390bb2f890347b2e0f0a642410a34f650537b
longvibol <vibollong@gmail.com>
2025-02-05 16:09:16 +0700

    week15_1

---
+++ Lesson/Week 15 Build and Ecommerce/Softcode/ecommerce-23-api/app/Http/Controllers/AuthController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Hash;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|confirmed',
+        ]);
+
+       if($request->hasFile('avatar'))//check if request has file
+       {
+        $avatar = $request->file('avatar');// client send the key as avatar
+        $avatar_path = Storage::disk('public')->put('users', $avatar);
+        $request->merge(['avatar' => $avatar_path]);
+       } 
+
+      $user = User::create([
+           'name' => $request->name,
+           'email' => $request->email,
+           'password' => Hash::make($request->password),
+           'avatar' => $request->avatar
+       ]);
+
+    // $user = User::create((array)$request->all());
+
+       $token = $user->createToken('auth_token')->plainTextToken;
+
+       return response()->json([
+           'access_token' => $token,
+           'token_type' => 'Bearer',
+           'message' => 'User created successfully',
+           'user' => $user
+       ]);       
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Invalid credentials',
+            ], 401);
+        }else{
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'access_token' => $token,
+                'token_type' => 'Bearer',
+                'message' => 'User logged in successfully',
+                'user' => $user,
+            ]);
+        }
+    }
+}

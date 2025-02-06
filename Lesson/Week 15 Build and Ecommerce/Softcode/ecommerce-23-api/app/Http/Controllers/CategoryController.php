749790782e99ed2417a8dcbeac76b5202c3e90e6
longvibol <vibollong@gmail.com>
2025-02-06 17:22:12 +0700

    wer

---
+++ Lesson/Week 15 Build and Ecommerce/Softcode/ecommerce-23-api/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    // we use to get all the categories from the database index 
+    public function index()
+    {
+        $categories = Category::all();
+        return response()->json([
+            'categories' => $categories
+        ]);
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'description' => 'required|string',
+        ]);
+
+        $category = Category::create($request->all());
+
+        return response()->json([
+            'message' => 'Category created successfully',
+            'category' => $category
+        ]);
+    }
+    public function update(Request $request,$id){
+        $category = Category::find($id);
+        if(!$category){
+            return response()->json([
+                'message' => 'Category not found',
+            ], 404);
+        }
+        $category->update($request->all());
+        return response()->json([
+            'message' => 'Category updated successfully',
+            'category' => $category
+        ]);
+    }
+
+    public function destroy($id){
+        $category = Category::find($id);
+        if(!$category){
+            return response()->json([
+                'message' => 'Category not found',
+            ], 404);
+        }
+        $category->delete();
+        return response()->json([
+            'message' => 'Category deleted successfully',
+        ]);
+    }
+}
--- Lesson/Week 15 Build and Ecommerce/Softcode/ecommerce-23-api/routes/api.php
+++ Lesson/Week 15 Build and Ecommerce/Softcode/ecommerce-23-api/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -10,3 +11,14 @@ Route::get('/user', function (Request $request) {
 
 Route::post('login',[AuthController::class,'login']);
 Route::post('register',[AuthController::class,'register']);
+
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::post('logout',[AuthController::class,'logout']);
+
+    // category routes
+   Route::get('categories',[CategoryController::class,'index']); // Get all categories
+    Route::post('categories',[CategoryController::class,'store']); // Create a new category
+    Route::post('categories/{id}',[CategoryController::class,'update']); // Update a category
+    Route::delete('categories/{id}',[CategoryController::class,'destroy']); // Delete a category
+});
